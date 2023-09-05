29d1ecbb4fcb41c0b666d9f9813df75e72b584e9
Per Enström <ext.per.enstrom@getswish.se>
2023-09-05 12:37:39 +0200

    Add types to contentful

--- with_styled_components/pages/projects/index.tsx
+++ with_styled_components/pages/projects/index.tsx
@@ -1,21 +1,64 @@
-import { client } from "lib/contentful/client"
+import { Entry, EntryFieldTypes, EntrySkeletonType } from 'contentful';
+import { client } from 'lib/contentful/client';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
-export default function ProjectsPage({ projects }) {
-  console.log(projects)
-  return <h1>{projects}</h1>
+// We need to specify the props of our component, we do this here
+// by using the `InferGetStaticPropsType` helper from Next, which
+// takes in the return type of `getStaticProps` and returns the
+// correct type for our component props
+export default function ProjectsPage({
+  projects
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(projects);
+  return (
+    <h1>
+      {projects.map((p) => (
+        <p>{p.fields.name}</p>
+      ))}
+    </h1>
+  );
 }
 
-export async function getStaticProps() {
-  const res = await client.getEntries({ content_type: 'project'})
-  const projects = await res.items.json()
- 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+/* These types should be in a types file somewhere else
+ * or even better, automatically generated
+ */
+
+// First we declare a type matching our Contentful Model,
+// `EntrySkeletonType` is a helper type that will create
+// the correct type for the `fields` property.
+// It takes in an object type of the fiels and the content
+// type id as generic parameters.
+type ProjectSkeleton = EntrySkeletonType<
+  {
+    name: EntryFieldTypes.Symbol;
+    description: EntryFieldTypes.Symbol;
+  },
+  'project'
+>;
+
+// To get the correct type for a specific entry we use
+// the `Entry` type, which takes in the skeleton we created
+// above and a modifier, in this case `undefined` since
+// we don't have any localizations
+type ProjectEntry = Entry<ProjectSkeleton, undefined>;
+
+/***** End types */
+
+// We need to type the props we are returning from `getStaticProps`
+// using the `GetStaticProps` from Next, that takes in a generic
+// type describing the props to our component
+export const getStaticProps: GetStaticProps<{
+  projects: ProjectEntry[];
+}> = async () => {
+  // We pass in our skeleton type to the contentful method
+  const res = await client.getEntries<ProjectSkeleton>({
+    content_type: 'project'
+  });
+  const projects = await res.items;
+
   return {
     props: {
-      projects,
-    },
-  }
-}
-
-
+      projects
+    }
+  };
+};
